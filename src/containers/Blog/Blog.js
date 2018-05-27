bc038687e2cdafe2d5c6a20d6e51fdb60730abf6
Santosh Marigowda <santosharakere@gmail.com>
2018-05-27 20:43:06 +0100

    conditional rendering or gaurd

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,9 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 class Blog extends Component {
+    state = {
+        auth: false
+    }
     render () {
         return (
             <div className="Blog">
@@ -30,7 +33,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={ () => { return <h1>Home...</h1> } } />
                 <Route path="/" render={ () => { return <h1>Home 2...</h1> } } /> */}
                 <Switch>
-                    <Route path="/new-post" exact component={NewPost} />
+                    { this.state.auth ? <Route path="/new-post" exact component={ NewPost } /> : null }
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                     {/* <Route path="/" component={Posts} /> */}
