a2fdce5e0b2578d0b77c8d94d1ab8eec96850926
Santosh Marigowda <santosharakere@gmail.com>
2018-05-28 05:55:42 +0100

    code splitting, lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     render () {
         return (
@@ -33,7 +38,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={ () => { return <h1>Home...</h1> } } />
                 <Route path="/" render={ () => { return <h1>Home 2...</h1> } } /> */}
                 <Switch>
-                    { this.state.auth ? <Route path="/new-post" exact component={ NewPost } /> : null }
+                    { this.state.auth ? <Route path="/new-post" exact component={ AsyncNewPost } /> : null }
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" exact to="/posts" />
                     <Route render={ () => <h1>Not Found</h1>} />
